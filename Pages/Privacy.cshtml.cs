4106597aae671e2abef758119e7cf3340d53e496
RXAGU <reubenagu4@gmail.com>
2026-06-18 14:28:48 +0100

    Add files via upload

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FFSmart.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
